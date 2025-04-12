f6eeb67508f5a67b032f43b86ad2e4d32fb3dd1b
Lucas Carozza <lucas.carozza@icloud.com>
2025-04-12 17:47:12 -0300

    Setup axios and @tanstack/react-query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,16 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { Toaster } from "sonner";
 import { ThemeProvider } from "./components/themes/ThemeProvider";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
 
 export function App() {
   return (
     <ThemeProvider storageKey="foodboard-theme" defaultTheme="system">
       <Toaster richColors />
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
